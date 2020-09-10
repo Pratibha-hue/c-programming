0ef0550e1173c63a8e19efcb0e9d61c78d2e4236
Pratibha bhole <66415382+Pratibha-hue@users.noreply.github.com>
2020-09-10 16:50:21 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
-main(){
-	int choice=30;
-	(choice>=20)?printf("you are above 20"):printf("you are under 20");
+int main(){
+	int a,b;
+	printf("Enter number a:");
+	scanf("%d",&a);
+	printf("Enter number b:");
+	scanf("%d",&b);
+	a= a-b;
+	b= a+b;
+	a= b-a;
+	printf("After swapping: a=%.2d\n",a);
+	printf("After swapping: b=%.2d",b);
 	return 0;
+
 }
